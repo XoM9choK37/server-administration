4460b45602d4e75bd9882a3fd33179b03680a3e4
XoM9choK37 <sanya.sasha.kostrov@mail.ru>
2026-03-13 02:29:12 +0300

    Add files

--- practice/bash/7.sh
+++ practice/bash/7.sh
@@ -3,14 +3,6 @@
 IFS=':' read -ra directories <<< "$PATH"
 
 for dir in "${directories[@]}"; do
-    if [ -z "$dir" ]; then
-        continue
-    fi
-    
-    if [ -d "$dir" ] && [ -r "$dir" ]; then
-        file_count=$(ls -l "$dir" 2>/dev/null | grep "^-" | wc -l)
-        echo "$dir: $file_count"
-    else
-        echo "$dir: директория не существует или недоступна"
-    fi
+    file_count=$(ls -l "$dir" | grep "^-" | wc -l)
+    echo "$dir=>$file_count"
 done
